f32b757c07f0171c314d6824e7af382e682aeaa2
Syl <syl@sperg.net>
2025-08-31 23:04:24 -0400

    -

--- KZeXA/keymap.c
+++ KZeXA/keymap.c
@@ -370,7 +370,6 @@ uint8_t layer_state_set_user(uint8_t state) {
 const key_override_t delete_key_override = 
     ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
-const key_override_t **key_overrides = (const key_override_t *[]){
-	&delete_key_override,
-	NULL
+const key_override_t *key_overrides[] = {
+	&delete_key_override
 };
